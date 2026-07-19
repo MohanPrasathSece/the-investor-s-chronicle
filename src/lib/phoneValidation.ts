457b1a3f8228e0c0fc242f4966f88921a119091b
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2026-07-19 20:18:10 +0530

    feat: add Ireland, Netherlands, Great Britain to country codes

--- src/lib/phoneValidation.ts
+++ src/lib/phoneValidation.ts
@@ -134,7 +134,9 @@ export const COUNTRIES: Country[] = [
     placeholder: "+357 99 123456",
     regex: /^\+?[1-9]\d{6,14}$/,
     errorMsg: "Please enter a valid phone number with dial code (7 to 15 digits)."
-  }
+  },
+  { code: "IE", name: "Ireland", dialCode: "+353", flag: "🇮🇪", placeholder: "87 123 4567", regex: /^\+353[89]\d{7,8}$/, errorMsg: "Irish number must be 8 or 9 digits." },
+  { code: "GBR", name: "Great Britain", dialCode: "+44", flag: "🇬🇧", placeholder: "7700 900077", regex: /^\+447\d{9}$/, errorMsg: "UK mobile number must be 10 digits." },
 ];
 
 export const getCountry = (code: string): Country => {
